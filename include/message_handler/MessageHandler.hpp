2d3ccc19ea63c119af82913a3b15e905fd0af302
Baldeep <baldeep.gill@kcl.ac.uk>
2026-01-28 23:08:19 +0000

    Change access modifiers in MessageHandler classes

--- include/message_handler/MessageHandler.hpp
+++ include/message_handler/MessageHandler.hpp
@@ -6,13 +6,11 @@
 
 class MessageHandler {
     public:
-        std::unordered_map<uint16_t, std::size_t> stock_count_;
-        
+        ~MessageHandler() = default;
+
         std::uint64_t bswap64(std::uint64_t n) { return __builtin_bswap64(n); }
         std::uint32_t bswap32(std::uint32_t n) { return __builtin_bswap32(n); }
         std::uint16_t bswap16(std::uint16_t n) { return __builtin_bswap16(n); }
-
-        virtual ~MessageHandler() = default;
         
         void handle_message(const ItchMessage& msg) {
             std::visit([&](auto&& m){ 
@@ -20,8 +18,10 @@ class MessageHandler {
                 process_message(m); 
             }, msg);
         };
-
+        
     private:
+        std::unordered_map<uint16_t, std::size_t> stock_count_;
+        
         virtual void process_message(const Messages::S_SystemEvent&) {}
         virtual void process_message(const Messages::R_StockDirectory&) {}
         virtual void process_message(const Messages::H_StockTradingAction&) {}
--- include/message_handler/OrderBookMessageHandler.hpp
+++ include/message_handler/OrderBookMessageHandler.hpp
@@ -4,23 +4,26 @@
 #include "orderbook/OrderBook.hpp"
 
 class OrderBookMessageHandler : public MessageHandler {
+    public:
+        OrderBookMessageHandler(OrderBook& book) : book_{book}, stock_locates_{} {};
+        ~OrderBookMessageHandler() = default;
+    
+        const std::string& get_stock(std::uint16_t locate); 
+
     private:
         OrderBook& book_;
         std::unordered_map<uint16_t, std::string> stock_locates_;
-
+        
         std::string trim_spaces(const char* s, size_t size);
         void add_order(std::uint64_t id, char side, std::uint32_t price, std::uint32_t qty);
-        
         
         void process_message(const Messages::R_StockDirectory&);
         void process_message(const Messages::A_AddOrder&);
         void process_message(const Messages::F_AddOrderMPID&);
-        void process_message(const Messages::E_OrderExecuted&);
-        void process_message(const Messages::C_OrderExecutedMessage&);
+        // void process_message(const Messages::E_OrderExecuted&);
+        // void process_message(const Messages::C_OrderExecutedMessage&);
         void process_message(const Messages::X_OrderCancel&);
         void process_message(const Messages::D_OrderDelete&);
         void process_message(const Messages::U_OrderReplace&);
-    
-    public:
-        OrderBookMessageHandler(OrderBook& book) : book_{book}, stock_locates_{} {};
+        
 };
