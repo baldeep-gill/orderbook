59c192c9741edb0dffc6672ee53b451d3136bab5
Baldeep <baldeep.gill@kcl.ac.uk>
2026-01-28 21:43:41 +0000

    Added overloads in OrderBookMessageHandler for Cancel, Delete, and Replace messages

--- src/MessageHandler.cpp
+++ src/MessageHandler.cpp
@@ -11,10 +11,10 @@ std::string OrderBookMessageHandler::trim_spaces(const char* s, size_t size) {
 
 void OrderBookMessageHandler::add_order(std::uint64_t id, char side, std::uint32_t price, std::uint32_t qty) {
     book_.add_order(
-        __builtin_bswap64(id),
+        bswap64(id),
         static_cast<Side>(side == 'S'),
-        __builtin_bswap32(price) / 10'000.0,
-        __builtin_bswap32(qty)
+        bswap32(price) / 10'000.0,
+        bswap32(qty)
     );
 }
 
@@ -30,3 +30,21 @@ void OrderBookMessageHandler::process_message(const Messages::A_AddOrder& msg) {
 void OrderBookMessageHandler::process_message(const Messages::F_AddOrderMPID& msg) {
     add_order(msg.order_reference, msg.side, msg.price, msg.shares);
 }
+
+void OrderBookMessageHandler::process_message(const Messages::X_OrderCancel& msg) {
+    book_.cancel_order(bswap64(msg.reference_number), bswap32(msg.shares_cancelled));
+}
+
+void OrderBookMessageHandler::process_message(const Messages::D_OrderDelete& msg) {
+    book_.delete_order(bswap64(msg.reference_number));
+}
+
+void OrderBookMessageHandler::process_message(const Messages::U_OrderReplace& msg) {
+    book_.replace_order(
+        bswap64(msg.original_reference),
+        bswap64(msg.new_reference),
+        bswap32(msg.price) / 10'000.0,
+        bswap32(msg.shares)
+    );
+}
+
