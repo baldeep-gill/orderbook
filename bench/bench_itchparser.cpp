87457ddd86f076ebcb79f6edd760e68d337475fe
Baldeep <baldeep.gill@kcl.ac.uk>
2026-02-11 15:27:39 +0000

    Implement benchmarking using Google benchmark

---
+++ bench/bench_itchparser.cpp
@@ -0,0 +1,94 @@
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <benchmark/benchmark.h>
+#include "itch_parser/ItchParser.hpp"
+#include "message_handler/MessageHandler.hpp"
+
+class NullHandler : MessageHandler {
+    public:
+        explicit NullHandler() : count{0} {};
+        ~NullHandler() = default;
+
+        void handle_message(ItchMessage& msg) {
+            count++;
+            benchmark::DoNotOptimize(msg);
+        }
+
+        size_t count;
+};
+
+class BenchItchParser : ItchParser<NullHandler> {
+    public:
+        BenchItchParser() : ItchParser<NullHandler>{std::make_unique<NullHandler>()} {}
+
+        std::vector<char> load_itch_file(const std::string& path) {
+            std::ifstream file(path, std::ios::binary | std::ios::ate);
+            if (!file) { std::cout << "Could not open file!" << std::endl; return {}; }
+
+            auto size = file.tellg();
+            file.seekg(0);
+
+            std::vector<char> buffer(static_cast<size_t>(size));
+            file.read(buffer.data(), size);
+            return buffer;
+        }
+
+        void parse_file(const std::vector<char>& buffer) {
+            std::uint16_t message_size;
+            size_t offset = 0;
+
+            while (offset + 2 <= buffer.size()) {
+                std::memcpy(&message_size, buffer.data() + offset, 2);
+                message_size = ntohs(message_size);
+                offset += 2;
+                if (offset + message_size > buffer.size()) break;
+
+                char message_type = buffer[offset];
+                offset += 1;
+
+                char message_buffer[message_size];
+                message_buffer[0] = message_type;
+
+                std::memcpy(message_buffer + 1, buffer.data() + offset, message_size - 1);
+
+                ItchMessage msg = parse_message(message_buffer, message_size, message_type);
+                message_handler_->handle_message(msg);
+
+                offset += message_size - 1;
+            }
+        }
+};
+
+
+
+static void BM_ItchParser_ParseFile(benchmark::State& state) {
+    std::string filename = "../data/S071321-v50.bin";
+    BenchItchParser parser{};
+
+    auto buffer = parser.load_itch_file(filename);
+
+    for (auto _ : state) {
+        parser.parse_file(buffer);
+    }
+
+    state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(buffer.size()));
+}
+
+static void BM_ItchParser_FileStream(benchmark::State& state) {
+    ItchParser<NullHandler> parser{std::make_unique<NullHandler>()};
+    
+    for (auto _ : state) {
+        parser.parse_file();
+        benchmark::DoNotOptimize(parser);
+    }
+    
+    state.counters["msgs"] = parser.get_handler().count;
+}
+
+BENCHMARK(BM_ItchParser_FileStream)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_ItchParser_ParseFile)->Unit(benchmark::kMillisecond);
---
+++ bench/bench_orderbook.cpp
@@ -0,0 +1,77 @@
+#include <random>
+#include <vector>
+
+#include <benchmark/benchmark.h>
+#include "orderbook/OrderBook.hpp"
+
+struct Command {
+    enum class Type { Add, Cancel } type;
+    std::uint64_t id;
+    Side side;
+    Price price;
+    Quantity quantity;
+};
+
+std::vector<Command> generate_work(size_t n_adds, size_t n_cancels) {
+    std::vector<Command> commands{};
+    commands.reserve(n_adds + n_cancels);
+
+    std::mt19937_64 rng{1738};
+    std::uniform_real_distribution<double> price_distribution{99.0, 101.0};
+    std::uniform_int_distribution<std::uint64_t> qty_distribution{1, 1000};
+
+    for (size_t i = 0; i < n_adds; ++i) {
+        Command c{
+            .type = Command::Type::Add,
+            .id = static_cast<std::uint64_t>(i + 1),
+            .side = static_cast<Side>(i % 2 == 0),
+            .price = price_distribution(rng),
+            .quantity = qty_distribution(rng)
+        };
+        commands.push_back(c);
+    }
+
+    std::uniform_int_distribution<size_t> id_idx{0, n_adds - 1};
+    for (size_t i = 0; i < n_cancels; ++i) {
+        auto idx = id_idx(rng);
+        commands.push_back({
+            .type = Command::Type::Cancel, 
+            .id = commands[idx].id, 
+            .quantity = commands[idx].quantity
+        });
+    }
+
+    return commands;
+}
+
+static void BM_OrderBook_AddAndCancel(benchmark::State& state) {
+    const size_t n_adds = static_cast<size_t>(state.range(0));
+    const size_t n_cancels = static_cast<size_t>(state.range(1));
+
+    auto commands = generate_work(n_adds, n_cancels);
+
+    for (auto _ : state) {
+        OrderBook book{};
+        benchmark::DoNotOptimize(&book);
+
+        for (const auto& c: commands) {
+            if (c.type == Command::Type::Add) {
+                book.add_order(c.id, c.side, c.price, c.quantity);
+            } else {
+                book.cancel_order(c.id, c.quantity);
+            }
+        }
+
+        state.PauseTiming();
+    }
+    
+    state.SetComplexityN(n_adds + n_cancels);
+    state.SetLabel("adds-and-cancels");
+}
+
+BENCHMARK(BM_OrderBook_AddAndCancel)
+    ->Unit(benchmark::kMillisecond)
+    ->Ranges({ {50'000, 100'000}, {250'000, 500'000} })
+    ->Apply([](::benchmark::Benchmark* b) {
+        b->Complexity(benchmark::oN);
+    });
